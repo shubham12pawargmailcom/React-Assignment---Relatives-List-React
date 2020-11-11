811a3ab5f793e2f9ad70485ccab9150f1f2d802a
shubham12pawargmailcom <shubham.12pawar@gmail.com>
2020-11-11 13:57:08 +0530

    done changes in list

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
+class App extends React.Component {
     render() {
 
         return(
             <div id="main">
                {/* Do not remove the main div */}
+        <ol key={"relativeList"} >
+            <li key={"relativeList1"}>Sunil Salunkhe</li>
+            <li key={"relativeList2"}>Kisan Sathe</li>
+            <li key={"relativeList3"}>Kundalik Bhosale</li>
+            <li key={"relativeList4"}>Moahn Patil</li>
+        </ol>
             </div>
         )
     }
